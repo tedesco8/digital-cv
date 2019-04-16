b09a5ffbc51bdbff17836dc1250d8afde7ee0fe2
Pablo Martin Tedesco Rodriguez <pmtr@localhost.localdomain>
2019-04-16 17:53:14 -0300

    codificado componente skills

---
+++ src/components/Skills.jsx
@@ -0,0 +1,16 @@
+import React from 'react'; 
+
+const Skills = () => (
+    <div className="Skills">
+       <div className="Skills-container">
+          <div className="Skills-item">
+             <h5>HTML</h5>
+             <div className="Skills-line">
+                <span>95%</span>
+             </div>
+          </div>
+       </div>
+    </div>
+);
+
+export default Skills;
