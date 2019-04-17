6aeb3f09966c1273b2bd99024cdb62b5d66cfc6e
Pablo Martin Tedesco Rodriguez <pmtr@localhost.localdomain>
2019-04-17 03:13:01 -0300

    creacion de hooks

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -7,9 +7,12 @@ import Education from '../components/Education';
 import Experience from '../components/Experience';
 import Certificates from '../components/Certificates';
 import Skills from '../components/Skills';
+import useGetData from '../hooks/useGetDara';
 
 //retornamos nuestra estructura
 const App = () => {
+    const data = useGetData();
+    console.log(data);
     return (
         <Main>
             <Sidebar>
