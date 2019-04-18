27982c1101b8956e817912fe019363426d0cc0ff
Pablo Martin Tedesco Rodriguez <pmtr@localhost.localdomain>
2019-04-17 23:48:47 -0300

    Estilos en About terminados

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Social from './Social';
 import styled from 'styled-components';
 
+//Estilos
 const AboutStyle = styled.div`
 text-align: center;
 `;
@@ -17,6 +18,33 @@ margin: 0 auto;
 display: block;
 box-shadow: 0 0 10px rgba(0,0,0,0.6);
 `;  
+const AboutName = styled.div`
+text-align: center;
+`; 
+const AboutH2 = styled.h2`
+font-family: 'Roboto', sans-serif;
+font-weight: 400;
+letter-spacing: 1.2px;
+margin: .5em 0 0 0;
+color: #C2185B;
+`; 
+const AboutProfession = styled.p`
+margin: .2em 0 1em 0;
+letter-spacing: 1.6px;
+font-weight: 300;
+color: #C2185B;
+`; 
+const AboutBio = styled.p`
+color: #757575;
+font-sixe: 1em;
+font-weight 300;
+`; 
+const AboutLocation = styled.p`
+color: #757575;
+font-sixe: 1em;
+font-weight 400;
+`; 
+
 const About = ({avatar, name, profession, bio, address, social}) => (
     <AboutStyle>
        <div className="About-container">
@@ -25,17 +53,17 @@ const About = ({avatar, name, profession, bio, address, social}) => (
                 <AboutImg src={avatar} alt="{name}" />
              </figure>
           </AboutAvatar>
-          <div className="About-name">
-             <h2>{name}</h2>
-          </div>
+          <AboutName>
+             <AboutH2>{name}</AboutH2>
+          </AboutName>
           <div className="About-profession">
-             <p>{profession}</p>
+             <AboutProfession>{profession}</AboutProfession>
           </div>
           <div className="About-desc">
-             <p>{bio}</p>
+             <AboutBio>{bio}</AboutBio>
           </div>
           <div className="About-location">
-             <p>{address}</p>
+             <AboutLocation>{address}</AboutLocation>
           </div>
           <div className="About-social">
              <Social social={social} />
