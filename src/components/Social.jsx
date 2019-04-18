e66d7d1ddb9c6b1cf5c6a0b2af2db838bd7ebe90
Pablo Martin Tedesco Rodriguez <pmtr@localhost.localdomain>
2019-04-18 13:43:20 -0300

    estilos a componente Social

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,19 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const SocialStyle = styled.div`
+margin: 0 auto;
+display: block;
+`;
+const SocialUl = styled.ul`
+list-style-type: none;
+margin: 0;
+padding: 0;
+`;
+const SocialLi = styled.li`
+display: inline;
+margin: 0 1em 0 0;
+`;
+const SocialAnchor = styled.a`
+color: #212121;
+text-decoration: none;
+font-size: 1.2em;
+`;
 
 const Social = props => (
-    <div className="Social">
+    <SocialStyle>
        {props.social &&
-       <ul>
+       <SocialUl>
            {props.social.map((item, index) => (
-               <li key={`social-${index}`} target="_blank">
-                  <a href={item.url}>
+               <SocialLi key={`social-${index}`} target="_blank">
+                  <SocialAnchor href={item.url}>
                   {item.name}
-                  </a>
-               </li>
+                  </SocialAnchor>
+               </SocialLi>
            ))}
-       </ul>
+       </SocialUl>
     }
-   </div>
+   </SocialStyle>
 );
 
 export default Social;
