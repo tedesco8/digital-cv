90e30ec9513395447cb9e5b5f264cdf491954a66
Pablo Martin Tedesco Rodriguez <pmtr@localhost.localdomain>
2019-04-18 19:27:03 -0300

    estilos componente Skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,18 +1,44 @@
 import React from 'react'; 
+import styled from 'styled-components';
+
+const SkillsContainer = styled.div`
+display: grid;
+grid-template-columns: repeat(3, 1fr);
+grid-gap: 2rem;
+grid-row-gap: 0.5em;
+`;
+const SkillsH5 = styled.h5`
+margin: .5em 0;
+`;
+const SkillsLine = styled.div`
+height: 8px;
+position: relative;
+border-radius: 50px;
+background: #F8BBD0;
+`;
+const SkillsSpan = styled.span`
+display: block;
+height: 100%;
+width: ${props => props.width};
+border-radius: 8px;
+background-color: #C2185B;
+overflow: hidden;
+position: relative;
+box-shadow: inset 0 2px 9px rgba(255,255,255,0.3), inset 0 -2px 6px rgba(0,0,0,0.4);
+`;
 
 const Skills = props => (
     <div className="Skills">
-       <div className="Skills-container">
-
+       <SkillsContainer>
        {props.data.map((skill, index) => (
          <div className="Skills-item" key={`Skills-${index}`}>
-             <h5>{skill.name}</h5>
-             <div className="Skills-line">
-                <span>{skill.percentage}</span>
+             <SkillsH5>{skill.name}</SkillsH5>
+             <SkillsLine>
+                <SkillsSpan width={skill.percentage}/>
+             </SkillsLine>
              </div>
-          </div>
        ))}
-       </div>
+       </SkillsContainer>
     </div>
 );
 
