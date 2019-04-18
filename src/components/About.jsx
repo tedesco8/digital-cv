4935ca2023763de384400fcc65bd59d77e4877b4
Pablo Martin Tedesco Rodriguez <pmtr@localhost.localdomain>
2019-04-17 23:23:45 -0300

    Estilizando con Styled Components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,30 @@
 import React from 'react'; 
 import Social from './Social';
+import styled from 'styled-components';
 
+const AboutStyle = styled.div`
+text-align: center;
+`;
+const AboutAvatar = styled.div`
+padding: 2em 0 0 0;
+`; 
+const AboutImg = styled.img`
+border-radius: 100%;
+width: 160px;
+height: 160px;
+border: 2px solid #E91E63;
+margin: 0 auto;
+display: block;
+box-shadow: 0 0 10px rgba(0,0,0,0.6);
+`;  
 const About = ({avatar, name, profession, bio, address, social}) => (
-    <div className="About">
+    <AboutStyle>
        <div className="About-container">
-          <div className="About-avatar">
+          <AboutAvatar>
              <figure>
-                <img src={avatar} alt="{name}" />
+                <AboutImg src={avatar} alt="{name}" />
              </figure>
-          </div>
+          </AboutAvatar>
           <div className="About-name">
              <h2>{name}</h2>
           </div>
@@ -25,7 +41,7 @@ const About = ({avatar, name, profession, bio, address, social}) => (
              <Social social={social} />
           </div>
        </div>
-    </div>
+    </AboutStyle>
 );
 
 export default About;
